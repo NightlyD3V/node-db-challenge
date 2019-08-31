79d1308d9e7e594ad26c6a8dc91f3c6e7423ba92
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-08-30 20:24:03 -0400

    Inital set-up of migration file

--- data/migrations/20190830200454_tasks.js
+++ data/migrations/20190830200454_tasks.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+    //🎨 TASKS TABLE  
+    return knex.schema
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.text('task_name', 128)
+            .notNullable();
+        tbl.integer('task_id')
+            .unique()
+            .notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('tasks');
 };
